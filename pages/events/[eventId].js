2224d04d7b611c76375058200ddc926b5bdd5101
Vadim Storozhuk <vadozy@yahoo.com>
2021-10-04 20:13:33 -0400

    End of Section 04 - File-based Routing Project

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,14 +1,40 @@
-import React from 'react';
+import { Fragment } from 'react';
 import { useRouter } from 'next/router';
 
-function EventDetailsPage() {
+import { getEventById } from '../../data/dummy-data';
+import EventSummary from '../../components/event-detail/event-summary';
+import EventLogistics from '../../components/event-detail/event-logistics';
+import EventContent from '../../components/event-detail/event-content';
+import ErrorAlert from '../../components/ui/error-alert';
+
+function EventDetailPage() {
   const router = useRouter();
+
   const eventId = router.query.eventId;
+  const event = getEventById(eventId);
+
+  if (!event) {
+    return (
+      <ErrorAlert>
+        <p>No event found!</p>
+      </ErrorAlert>
+    );
+  }
+
   return (
-    <div>
-      <h1>The Event [{eventId}] Details Page</h1>
-    </div>
+    <Fragment>
+      <EventSummary title={event.title} />
+      <EventLogistics
+        date={event.date}
+        address={event.location}
+        image={event.image}
+        imageAlt={event.title}
+      />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </Fragment>
   );
 }
 
-export default EventDetailsPage;
+export default EventDetailPage;
