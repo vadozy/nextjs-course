06a9ecd1a05c1fd11b625974491acbde780fcf02
Vadim Storozhuk <vadozy@yahoo.com>
2021-10-11 22:59:17 -0400

    Section 09 - wip - Starting Setup [ after 149 ]

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Head from 'next/head';
 
 import EventList from '../components/events/event-list';
 import { getFeaturedEvents } from '../helpers/api-utils';
+import NewsletterRegistration from '../components/input/newsletter-registration';
 
 function HomePage(props) {
   const { featuredEvents } = props;
@@ -13,6 +14,7 @@ function HomePage(props) {
         <meta name="description" content="Some Great Events" />
       </Head>
       <div>
+        <NewsletterRegistration />
         <EventList items={featuredEvents} />
       </div>
     </>
